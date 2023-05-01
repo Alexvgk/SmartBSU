d175ab19279eff305ed1dff7f2665b006a6e07d4
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-05-01 19:07:55 +0300

    changed model structure

--- VocabulartApp/Models/User.cs
+++ VocabulartApp/Models/User.cs
@@ -5,20 +5,22 @@ using System.Collections.Generic;
 namespace SmartBSU.Models
 {
     [Serializable]
-    public class Person : IEquatable<Person>
+    public class User : IEquatable<User>
     {
         public long Id { get; set; }
         public string FirstName { get; set; }
         public string SecondName { get; set; }
         public string Email { get; set;}
         public string Uid { get; set;}
+        public int IdRegCode { get; set;}
+        public RegCode RegCode { get; set;}
 
         public override bool Equals(object obj)
-        {
-            return Equals(obj as Person);
+        { 
+            return Equals(obj as User);
         }
 
-        public bool Equals(Person other)
+        public bool Equals(User other)
         {
             return !(other is null) &&
                    Id == other.Id &&
@@ -38,5 +40,13 @@ namespace SmartBSU.Models
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Uid);
             return hashCode;
         }
+    }
+
+    public class RegCode
+    {
+        public int IdRegistCode { get; set; }
+        public string RegistCode { get; set; }
+
+        public User User { get; set; }
     }
 }

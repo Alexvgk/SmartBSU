29ee35d223b1ac3780544d1db470bf768f94c710
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-05-01 19:12:43 +0300

    added context for EF and configuration of DB

--- VocabulartApp/Services/Data/MyDbContext.cs
+++ VocabulartApp/Services/Data/MyDbContext.cs
@@ -1,23 +1,35 @@
 ﻿using Android.App;
+using Android.Content;
 using Microsoft.EntityFrameworkCore;
+using SmartBSU.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SmartBSU.Services.Data
 {
-    public class UserContext : DbContext
+    public class MyDbContext : DbContext
     {
-        public DbSet<Models.Person> Persons { get; set; } = null!;
+        public DbSet<Models.User> users { get; set; } = null!;
+        public DbSet<RegCode> RegistCodes { get; set; } = null!;
 
-        public UserContext()
+        public MyDbContext()
         {
             Database.EnsureCreated();
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RegCode>()
+        .HasKey(r => r.IdRegistCode);
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySql("server=128.140.32.175;user=bsuuser;password=bsuuser;database=smartBSU;",
                 new MySqlServerVersion(new Version(8, 0, 28)));
+
         }
     }
 }
---
+++ VocabulartApp/Services/Data/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SmartBSU.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartBSU.Services.Data
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasOne(u => u.RegCode)
+                .WithOne(r => r.User)
+                .HasForeignKey<User>(u => u.IdRegCode);
+        }
+    }
+}
